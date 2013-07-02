829883e0bcd329b9cf303c34fc8896c0896feec8
apodes <aaronpodell@gmail.com>
2013-07-02 16:45:03 -0400

    Finish sign in - chapter 8

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -11,14 +11,15 @@
 
 require 'spec_helper'
 
-describe User do
+describe "User" do
   #pre-loaded test
   #pending "add some examples to (or delete) #{__FILE__}"
 
   before do
     @user = User.new(name: "Example User", email: "user@example.com", 
                     password: "foobar", password_confirmation: "foobar")
-end
+  end
+ 
   subject { @user }
 
   it { should respond_to(:name) }
@@ -26,6 +27,7 @@ end
   it { should respond_to(:password_digest) }
   it { should respond_to(:password) }
   it { should respond_to(:password_confirmation) }
+  it { should respond_to(:remember_token) }
   it { should respond_to(:authenticate) }
 
   it { should be_valid }
@@ -45,7 +47,7 @@ end
     it { should_not be_valid }
   end
 
-  describe "testaaaaaaaaaaa to make sure @user resets" do
+  describe "test to make sure @user resets" do
     it { should be_valid }
   end
 
@@ -60,71 +62,75 @@ end
     end
   end
 
-    describe "when email format is valid" do
-        it "should be valid" do
-            addresses = %w[user@foo.COM A_US-ER@f.b.org frst.lst@foo.jp a+b@baz.cn]
-            addresses.each do |valid_address|
-                @user.email = valid_address
-                @user.should be_valid
-            end      
-        end
-    end
+  describe "when email format is valid" do
+      it "should be valid" do
+          addresses = %w[user@foo.COM A_US-ER@f.b.org frst.lst@foo.jp a+b@baz.cn]
+          addresses.each do |valid_address|
+              @user.email = valid_address
+              @user.should be_valid
+          end      
+      end
+  end
 
 
-    describe "when email address is already taken" do
-        before do
-            user_with_same_email = @user.dup
-            user_with_same_email.email = @user.email.upcase
-            user_with_same_email.save
-        end
+  describe "when email address is already taken" do
+      before do
+          user_with_same_email = @user.dup
+          user_with_same_email.email = @user.email.upcase
+          user_with_same_email.save
+      end
 
-        it { should_not be_valid }
-    end
+      it { should_not be_valid }
+  end
 
-    describe "when password is not present" do
-        before { @user.password = @user.password_confirmation = " " }
-        it { should_not be_valid }
-    end
+  describe "when password is not present" do
+      before { @user.password = @user.password_confirmation = " " }
+      it { should_not be_valid }
+  end
 
-    describe "when password doesn't match confirmation" do
-        before { @user.password_confirmation = "mismatch" }
-        it { should_not be_valid }
-    end
+  describe "when password doesn't match confirmation" do
+      before { @user.password_confirmation = "mismatch" }
+      it { should_not be_valid }
+  end
 
-    describe "when password_confirmation is nil" do
-        before { @user.password_confirmation = nil }
-        it { should_not be_valid }
-    end
+  describe "when password_confirmation is nil" do
+      before { @user.password_confirmation = nil }
+      it { should_not be_valid }
+  end
 
-    describe "return value of authenticate method" do
-        before { @user.save }
-        let(:found_user) { User.find_by_email(@user.email) } 
+  describe "return value of authenticate method" do
+      before { @user.save }
+      let(:found_user) { User.find_by_email(@user.email) } 
 
-        describe "with valid password" do
-            it { should == found_user.authenticate(@user.password) }
-        end
+      describe "with valid password" do
+          it { should == found_user.authenticate(@user.password) }
+      end
 
-        describe "with invalid password" do
-            let(:user_for_invalid_password) { found_user.authenticate("invalid") }
+      describe "with invalid password" do
+          let(:user_for_invalid_password) { found_user.authenticate("invalid") }
 
-            it { should_not == user_for_invalid_password }
-            specify { user_for_invalid_password.should be_false }
-        end
-    end
+          it { should_not == user_for_invalid_password }
+          specify { user_for_invalid_password.should be_false }
+      end
+  end
 
-    describe "with a password that's too short" do
-        before { @user.password = @user.password_confirmation = "a" * 5 }
-        it { should be_invalid } #could also use {should_not be_valid}
-    end
+  describe "with a password that's too short" do
+      before { @user.password = @user.password_confirmation = "a" * 5 }
+      it { should be_invalid } #could also use {should_not be_valid}
+  end
 
-    describe "that emails are downcased before saving" do
-       let(:mixed_case_email) { "Foo@ExAmpLE.CoM"}
-        it "should be saved all lower-case" do
-            @user.email = mixed_case_email
-            @user.save
-            @user.reload.email.should == mixed_case_email.downcase
-        end
-    end
+  describe "that emails are downcased before saving" do
+     let(:mixed_case_email) { "Foo@ExAmpLE.CoM"}
+      it "should be saved all lower-case" do
+          @user.email = mixed_case_email
+          @user.save
+          @user.reload.email.should == mixed_case_email.downcase
+      end
+  end
 
+  describe "remember token" do
+    before { @user.save }
+    its(:remember_token) { should_not be_blank }
+  end
 end 
 
