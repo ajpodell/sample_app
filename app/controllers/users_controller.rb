0037b016974c7c38cc93cd35deca1b41ec32afbf
apodes <aaronpodell@gmail.com>
2013-07-10 16:34:01 -0400

    up through 10.3.1

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,7 @@ class UsersController < ApplicationController
 
     def show
         @user = User.find(params[:id])
+        @microposts = @user.microposts.paginate(page: params[:page])
     end
 
     def new
@@ -57,12 +58,14 @@ class UsersController < ApplicationController
 
     private
 
-        def signed_in_user
-            unless signed_in?
-                store_location
-                redirect_to signin_url, notice: "Please sign in." 
-            end
-        end
+        #moved to sessions helper so everything has access
+        
+        # def signed_in_user
+        #     unless signed_in?
+        #         store_location
+        #         redirect_to signin_url, notice: "Please sign in." 
+        #     end
+        # end
 
         def correct_user
             @user = User.find(params[:id])
